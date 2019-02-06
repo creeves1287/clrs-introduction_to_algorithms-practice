d828538011453c71172f083000e6c5e6b2d1e44f
creeves1287 <45729008+creeves1287@users.noreply.github.com>
2019-02-06 14:32:54 -0800

    updated merge sorter in place

---
+++ MergeSorter/MergeSorter/MergeSorterInPlace.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSorter
+{
+    public class MergeSorterInPlace : IMergeSorter
+    {
+        public void MergeSort(int[] numbers, int i, int j)
+        {
+            if (j > i)
+            {
+
+                MergeSort(numbers, i, (i + j) / 2);
+                MergeSort(numbers, (i + j) / 2 + 1, j);
+                Merge(numbers, i, j);
+            }
+        }
+
+        private void Merge(int[] numbers, int i, int j)
+        {
+            int mid = (i + j) / 2;
+            int left = i;
+            int right = mid + 1;
+
+            while (left < right)
+            {
+                if (numbers[right] < numbers[left])
+                {
+                    Swap(numbers, left, right);
+
+                    int k = right + 1;
+                    while (k <= j && numbers[k] < numbers[right])
+                    {
+                        Swap(numbers, right, k);
+                        k++;
+                    }
+                }
+                left++;
+            }
+
+            while (right <= j)
+            {
+                if (numbers[right] < numbers[left])
+                {
+                    Swap(numbers, left, right);
+                }
+                right++;
+            }
+        }
+
+        private void Swap(int[] numbers, int i, int j)
+        {
+            int tmp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = tmp;
+        }
+
+        //Time Complexity: O(n^2lgn), Space Complexity: O(n)
+    }
+}
--- MergeSorter/MergeSorterTests/MergeSorterTests.cs
+++ MergeSorter/MergeSorterTests/MergeSorterTests.cs
@@ -10,23 +10,47 @@ namespace MergeSorterTests
         [TestMethod]
         public void MyMergeSortTest()
         {
-            IMergeSorter mergeSorter = new MyMergeSorter();
+            IMergeSorter mergeSorter = new MergeSorterInPlace();
             RunTests(mergeSorter);
         }
 
         private void RunTests(IMergeSorter mergeSorter)
         {
             MergeSortTest(mergeSorter);
+            MergeSortRandomArrayTest(mergeSorter);
         }
 
         private void MergeSortTest(IMergeSorter mergeSorter)
         {
-            int[] a = new int[] { 3, 7, 6, 2, 5, 9, 4, 8, 1 };
-            mergeSorter.MergeSort(a, 0, a.Length);
+            int[] a = new int[] { 3, 7, 6, 2, 5, 9, 4, 8 };
+            mergeSorter.MergeSort(a, 0, a.Length - 1);
             for (int i = 0; i < a.Length; i++)
             {
-                Assert.AreEqual(i + 1, a[i]);
+                Assert.AreEqual(i + 2, a[i]);
             }
+        }
+
+        private void MergeSortRandomArrayTest(IMergeSorter mergeSorter)
+        {
+            int[] a = GenerateRandomArray(100);
+            int[] expected = (int[])a.Clone();
+            Array.Sort(expected);
+            mergeSorter.MergeSort(a, 0, a.Length - 1);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Assert.AreEqual(expected[i], a[i]);
+            }
+        }
+
+        private int[] GenerateRandomArray(int size)
+        {
+            int[] a = new int[size];
+            Random r = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = r.Next();
+            }
+            return a;
         }
     }
 }
